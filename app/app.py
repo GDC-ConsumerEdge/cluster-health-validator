f5ac00c6bb168e884807220ee3174121d6002e19
Ben Fogel <bfogel@google.com>
2024-07-24 05:26:53 +0000

    pylint updates for app.py
    Change-Id: I199bb3e39ab67da23d8f4b9d3d33901403dcb64c

--- app/app.py
+++ app/app.py
@@ -1,53 +1,57 @@
-from flask import Flask, abort
-from apscheduler.schedulers.background import BackgroundScheduler
-from kubernetes import config
-from kubernetes.client.exceptions import ApiException
-from prometheus_client import generate_latest, Gauge
+import concurrent.futures
 import logging
 import os
-import concurrent.futures
 import requests
-
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers import base
 from check_data_volumes import CheckDataVolumes
-from check_robin_cluster import CheckRobinCluster
 from check_node import CheckNode
+from check_robin_cluster import CheckRobinCluster
 from check_root_syncs import CheckRootSyncs
 from check_virtual_machines import CheckVirtualMachines
 from check_vmruntime import CheckVMRuntime
+from flask import Flask, abort
 from health_checks import HealthCheck
+from kubernetes import config
+from kubernetes.client.exceptions import ApiException
+from prometheus_client import Gauge, generate_latest
 
-logging.basicConfig(level=os.environ.get('LOG_LEVEL', 'INFO').upper())
+logging.basicConfig(level=os.environ.get("LOG_LEVEL", "INFO").upper())
 
 app = Flask(__name__)
 
-platform_health_metric = Gauge('platform_health', 'Platform Checks')
-workload_health_metric = Gauge('workload_health', 'Workload Checks')
+platform_health_metric = Gauge("platform_health", "Platform Checks")
+workload_health_metric = Gauge("workload_health", "Workload Checks")
 
-_MAX_WORKERS = os.environ.get('MAX_WORKERS', 10)
-_ROBIN_MASTER_SVC_ENDPOINT = 'robin-master.robinio.svc.cluster.local'
+_MAX_WORKERS = os.environ.get("MAX_WORKERS", 10)
+_ROBIN_MASTER_SVC_ENDPOINT = "robin-master.robinio.svc.cluster.local"
 _ROBIN_MASTER_SVC_METRICS_PORT = 29446
 
-@app.route('/metrics')
+
+@app.route("/metrics")
 def metrics():
+    """Prometheus metrics endpoint for workload and platform checks"""
     return generate_latest()
 
-@app.route('/robin_metrics')
+
+@app.route("/robin_metrics")
 def robin_metrics():
     """Queries and returns robin metrics available from the robin-master service.
-        This endpoint serves up robin metrics on an http endpoint, which allows
-        prometheus scraping from stackdriver.
+    This endpoint serves up robin metrics on an http endpoint, which allows
+    prometheus scraping from stackdriver.
     """
-    url = 'https://%s:%i/metrics' % (_ROBIN_MASTER_SVC_ENDPOINT, _ROBIN_MASTER_SVC_METRICS_PORT)
-    response = requests.get(url, verify=False)
+    url = f"https://{_ROBIN_MASTER_SVC_ENDPOINT}:{_ROBIN_MASTER_SVC_METRICS_PORT}/metrics"
+    response = requests.get(url, verify=False, timeout=10)
     return response.text
+
 
 def create_health_check_cr():
     """To create health check resource. It is invoked for each healthcheck
-    schedule as configsync apply may create pod first and clusterrolebinding 
+    schedule as configsync apply may create pod first and clusterrolebinding
     later which makes the CR creation fail."""
     try:
         return HealthCheck()
-    except Exception as e:
+    except Exception: # pylint: disable=broad-except
         logging.error("Failed to setup healthcheck CR", exc_info=True)
         logging.error("Health status will not updated in k8s CR")
     return None
@@ -68,8 +72,7 @@ def run_checks():
         CheckDataVolumes(),
         CheckVirtualMachines(),
     ]
-    with concurrent.futures.ThreadPoolExecutor(
-            max_workers=_MAX_WORKERS) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=_MAX_WORKERS) as executor:
         platform_checks_futures = {
             executor.submit(check.is_healthy): check.__class__.__name__
             for check in platform_checks
@@ -86,7 +89,7 @@ def run_checks():
                 try:
                     if not future.result():
                         checks_failed.append(name)
-                # Handling k8s resource not found here as it is not 
+                # Handling k8s resource not found here as it is not
                 # handled in the individual checks.
                 except ApiException as e:
                     if e.status == 404:
@@ -112,23 +115,25 @@ def run_checks():
             workload_health_metric.set(1)
 
         if health_check_cr:
-            health_check_cr.update_status(platform_checks_failed,
-                                          workload_checks_failed)
+            health_check_cr.update_status(
+                platform_checks_failed, workload_checks_failed
+            )
 
 
 config.load_config()
 health_check_cr = create_health_check_cr()
 
 scheduler = BackgroundScheduler(daemon=True)
-scheduler.add_job(run_checks, 'interval', minutes=1)
+scheduler.add_job(run_checks, "interval", minutes=1)
 scheduler.start()
 
-@app.route('/health')
+
+@app.route("/health")
 def health():
     """health endpoint that confirms the health check scheduler is running"""
 
     # 0 == stopped, 1 == running, 2 == paused
-    if scheduler.state == 1:
+    if scheduler.state == base.STATE_RUNNING:
         return "Ok"
     else:
         abort(500, "Scheduler not running")
