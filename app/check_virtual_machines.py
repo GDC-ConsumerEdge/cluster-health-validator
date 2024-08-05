f2e67b94358ed106dd0f13a1adc23478468e5f5b
Ben Fogel <bfogel@google.com>
2024-08-05 22:13:15 +0000

    Use GVM resource for health check instead of Kubevirt resource
    Change-Id: Id111081d91ca6af948776297bbf761757b3c091f

--- app/check_virtual_machines.py
+++ app/check_virtual_machines.py
@@ -6,24 +6,16 @@ log = logging.getLogger('check.virtualmachines')
 class CheckVirtualMachines:
     def is_healthy(self):
         k8s = client.CustomObjectsApi()
-        resp = k8s.list_namespaced_custom_object(group="kubevirt.io", version="v1", plural="virtualmachines", namespace="vm-workloads")
+        resp = k8s.list_namespaced_custom_object(group="vm.cluster.gke.io", version="v1", plural="virtualmachines", namespace="vm-workloads")
 
         # Expect multiple virtualmachines
         if (len(resp.get("items")) < 2):
             log.error(f'Found {len(resp.get("items"))} virtualmachines but expected 2 or more.')
             return False
-        
-        # Assert that each virtualmachine is ready
+
+        # Assert that each virtualmachine is running
         for virtual_machine in resp.get("items"):
-            if (virtual_machine.get("status").get("created") != True):
-                log.error(f'VirtualMachine {virtual_machine.get("metadata").get("name")} not created')
-                return False
-
-            if (virtual_machine.get("status").get("ready") != True):
-                log.error(f'VirtualMachine {virtual_machine.get("metadata").get("name")} not ready')
-                return False
-
-            if (virtual_machine.get("status").get("printableStatus") != "Running"):
+            if (virtual_machine.get("status").get("state") != "Running"):
                 log.error(f'VirtualMachine {virtual_machine.get("metadata").get("name")} not running')
                 return False
 
