56d027795775e2c308321355bc01827365e7c1bc
Mike Ensor <ensor@google.com>
2025-05-14 10:48:07 -0700

    initial commit of changes

---
+++ build.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+VERSION="$1"
+APP="${2}"  # usually ${CONTAINER_NAME}
+DOCKERFILE="${3:-Dockerfile}"
+
+function setup_project() {
+  # Create a .envrc file
+  # check if file .envrc exists
+  if [[ -f ".envrc" ]]; then
+    echo "Environment file .envrc already exists, skipping setting up .envrc"
+  fi
+}
+
+function display_common() {
+  echo "Typical Containers:"
+  echo "  * ${CONTAINER_NAME}"
+}
+
+function build_container() {
+  if [[ -f ".npmrc" ]]; then
+    docker build -f "${DOCKERFILE}" -t "${APP}:${VERSION}" . --secret id=npmrc,src=./.npmrc
+  else
+    docker build -f "${DOCKERFILE}" -t "${APP}:${VERSION}" . --secret id=npmrc,src=./.npmrc
+  fi
+
+  if [[ $? -ne 0 ]]; then
+      echo "Cannot build docker"
+      exit 1
+  fi
+}
+
+function release_container() {
+  docker tag "${APP}:${VERSION}" "${IMAGE_URL}:${VERSION}"
+  # shellcheck disable=SC2181
+  if [[ $? -ne 0 ]]; then
+      echo "Cannot Tag Docker Build with version"
+      exit 1
+  fi
+  docker tag "${APP}:${VERSION}" "${IMAGE_URL}:latest"
+  # shellcheck disable=SC2181
+  if [[ $? -ne 0 ]]; then
+      echo "Cannot Tag Docker Build with latest"
+      exit 1
+  fi
+  docker push "${IMAGE_URL}:${VERSION}"
+  # shellcheck disable=SC2181
+  if [[ $? -ne 0 ]]; then
+      echo "Cannot Push Version Tagged"
+      exit 1
+  fi
+  docker push "${IMAGE_URL}:latest"
+  # shellcheck disable=SC2181
+  if [[ $? -ne 0 ]]; then
+      echo "Cannot Push Latest Tagged"
+      exit 1
+  fi
+}
+
+function create_registry_url() {
+  if [[ -z "${IMAGE_URL}" ]]; then
+    export IMAGE_URL="registry.gitlab.com/${SCM_PROJECT}/${APP}"
+  fi
+}
+
+function validate_main() {
+  if [[ -z "${APP}" ]]; then
+    echo "Usage: ./build.sh <VERSION> <container> <APP-optional>"
+    exit 1
+  fi
+
+  REQUIRED_VARIABLES=( "APP" "VERSION" "SCM_PROJECT" )
+  # shellcheck disable=SC2043
+  HAS_ERROR=0
+  for REQ in "${REQUIRED_VARIABLES[@]}"
+  do
+    if [[ -z "${REQ}" ]]; then
+      echo "Variable ${REQ} is required and does not have a value"
+      HAS_ERROR=1
+    fi
+  done
+
+  if [[ ${HAS_ERROR} -gt 0 ]]; then
+    echo "One or more required variables are not set. Please set and re-run."
+    exit 1
+  fi
+}
+
+
+validate_main
+setup_project
+display_common
+create_registry_url
+
+echo "Building ${IMAGE_URL}:${VERSION}..."
+
+build_container
+# Add question if releasing
+
+# Confirm the release intention
+echo "Do you want to release v${new_version}? (y/n)"
+read -r response
+
+case "$response" in
+    [yY][eE][sS]|[yY]|[yY][aA])
+        echo "Releasing ${IMAGE_URL} v${new_version}..."
+        release_container
+        ;;
+    *)
+        echo "Operation cancelled."
+        exit 1
+        ;;
+esac
---
+++ generate.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+SPECIFIC_OVERLAY=$1
+OVERLAYS_BASE="overlays" # Adjust if your base path is different
+PACKAGE_FOLDER="package"
+PACKAGE_PREFIX="${2:-gdc-cluster-health-pkg-}"
+PACKAGE_PREFIX_SHORT="gdc-cluster"
+
+
+if [[ -z "${SPECIFIC_OVERLAY}" ]]; then
+    echo "Building all overlays"
+    rm -rf "${PACKAGE_FOLDER}"
+    mkdir -p "${PACKAGE_FOLDER}"
+
+    # Find all directories within the overlays base
+    for overlay_dir in "$OVERLAYS_BASE"/*/; do
+        # Extract the directory name without the trailing slash
+        overlay_name="${overlay_dir%/}"
+        overlay_name="${overlay_name##*/}"
+
+        if [[ -f "$(pwd)/${OVERLAYS_BASE}/${overlay_name}/kustomization.yaml" ]]; then
+            echo "Building overlay: $overlay_name" # Optional for progress updates
+            if [[ ! -d "$(pwd)/config/${overlay_name}" ]]; then
+                echo "Creating config directory for ${overlay_name}: $(pwd)/config/${overlay_name}"
+                mkdir -p "$(pwd)/config/${overlay_name}"
+            fi
+
+            # Run the kustomize build command (KGR)
+            kustomize build ${OVERLAYS_BASE}/$overlay_name ${HELM_CLI_FLAG} > config/$overlay_name/${PACKAGE_PREFIX_SHORT}$overlay_name-generated.yaml
+            kustomize build ${OVERLAYS_BASE}/$overlay_name ${HELM_CLI_FLAG} > package/${PACKAGE_PREFIX}$overlay_name.yaml
+        else
+            echo "No '${OVERLAYS_BASE}/${overlay_name}/kustomization.yaml' file found in overlay: $overlay_name. Skipping..."
+            continue
+        fi
+    done
+else
+    echo "Building overlay: $SPECIFIC_OVERLAY"
+    kustomize build overlays/$SPECIFIC_OVERLAY > config/$SPECIFIC_OVERLAY/$SPECIFIC_OVERLAY-generated.yaml
+fi
